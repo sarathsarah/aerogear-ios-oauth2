96e627f3f8f72004b0f71315285ed4359e711597
Sarathkumar <sarathkumar.d@tatacommunications.com>
2022-03-31 17:35:48 +0530

    SPM initial commmit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "AeroGearOAuth2",
+    products: [
+        .library(
+            name: "AeroGearOAuth2",
+            targets: ["AeroGearOAuth2"]),
+    ],
+    dependencies: [
+        .package(name: "AeroGearHttp", url: "https://github.com/sarathsarah/aerogear-ios-http.git", branch: "master"),
+    ],
+    targets: [
+        .target(
+            name: "AeroGearOAuth2",
+            dependencies: ["AeroGearHttp"],
+            path: "AeroGearOAuth2",
+            exclude: ["info.plist"]
+        )
+    ]
+)
